4587990388eaedb76f0469f7f0a785174f809f10
ayeshayusufgit <ayesha.yusuf@gmail.com>
2023-01-29 17:39:56 +0530

    Adding GitDemo class

---
+++ src/main/java/com/democart/qa/selenium/factory/GitDemo.java
@@ -0,0 +1,12 @@
+package com.democart.qa.selenium.factory;
+
+public class GitDemo {
+
+	public void gitPush() {
+		System.out.println("Git Push");
+	}
+	
+	public void gitPull() {
+		System.out.println("Git Pull");
+	}
+}
