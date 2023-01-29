4898961011f375bfc958b937f50dea6505a26495
ayeshayusufgit <ayesha.yusuf@gmail.com>
2023-01-29 19:12:56 +0530

    updating the files

--- src/main/java/com/democart/qa/selenium/pages/ProfilePage.java
+++ src/main/java/com/democart/qa/selenium/pages/ProfilePage.java
@@ -1,5 +1,14 @@
 package com.democart.qa.selenium.pages;
 
+import org.openqa.selenium.By;
+
 public class ProfilePage {
 
+	//Profile Icon
+	By profileIcon=By.id("profile");
+	
+	public void updatingProfile() {
+		System.out.println("Updated Profile!");
+	}
+	
 }
