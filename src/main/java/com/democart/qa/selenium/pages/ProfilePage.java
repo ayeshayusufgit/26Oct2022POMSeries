5f71fd1973fce34f184d5ec39958ea5c143dbbb3
ayeshayusufgit <ayesha.yusuf@gmail.com>
2023-01-29 19:21:29 +0530

    updating the Profile

--- src/main/java/com/democart/qa/selenium/pages/ProfilePage.java
+++ src/main/java/com/democart/qa/selenium/pages/ProfilePage.java
@@ -5,10 +5,16 @@ import org.openqa.selenium.By;
 public class ProfilePage {
 
 	//Profile Icon
-	By profileIcon=By.id("profile");
+	By profileIcon=By.id("profileIcon");
+	By deleteIcon=By.id("removeIcon");
 	
 	public void updatingProfile() {
 		System.out.println("Updated Profile!");
+	}
+	
+	
+	public void deleteProfile() {
+		System.out.println("Delete Profile!");
 	}
 	
 }
